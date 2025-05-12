0f5e903b10a6bc7e7b1457dcc31cef72748cd60d
chrowland <chrisrow@zillowgroup.com>
2025-05-12 12:55:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ elif trend_type == "Quadratic":
     #intercept = st.sidebar.slider("Intercept", min_value=5.0, max_value=50.0, value=10.0, step=1.0)
     a = st.sidebar.slider("Coefficient a", min_value=0.0, max_value=2.0, value=0.01, step=0.01)
     b = st.sidebar.slider("Coefficient b", min_value=0.0, max_value=10.0, value=0.5, step=0.1)
-    st.sidebar.subheader("$trend = intercept + a*X^2 +b*X$")
+    st.sidebar.subheader("$trend = int + a*X^2 +b*X$")
 
 # Cycle parameters
 st.sidebar.subheader("Cycle Component")
